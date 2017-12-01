a024241d3224fba733a32936f8081280b4d5becf
Andrei Kazakou <akazakou@exadel.com>
2017-12-01 18:28:22 +0300

    Revert database changes in connection section
    Signed-off-by: Andrei Kazakou <akazakou@exadel.com>

--- src/services/database/index.ts
+++ src/services/database/index.ts
@@ -8,19 +8,16 @@ const config = Config.init();
 // initializing logger instance
 const log = Log.init();
 
-export function init(): Promise<Mongoose.Connection> {
-  return new Promise((resolve, reject) => {
-    (<any>Mongoose).Promise = Promise;
+export async function init(): Promise<void> {
 
-    let connectionString: string = config.get('database:uri');
-    let connection = Mongoose.createConnection(connectionString, config.get('database:options'));
-    connection.once('open', () => {
-      log.info(`Connected to database: ${connectionString}`);
-      resolve(connection);
-    });
-    connection.once('error', (error) => {
-      log.error(`Unable to connect to database: ${connectionString}`);
-      reject(error);
-    });
-  });
+  (<any>Mongoose).Promise = Promise;
+
+  let connectionString: string = config.get('database:uri');
+
+  try {
+    await Mongoose.connect(connectionString, config.get('database:options'));
+    log.info(`Connected to database: ${connectionString}`);
+  } catch (error) {
+    log.error(`Unable to connect to database: ${connectionString}`);
+  }
 }
