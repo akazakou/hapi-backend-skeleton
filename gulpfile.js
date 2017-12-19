11c17bd684986db4a86f4af15eb16d5540cd37bd
Andrei Kazakou <akazakou@exadel.com>
2017-12-19 18:26:56 +0300

    Improove test coverage CI

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const gulp = require('gulp');
-const istanbul = require('gulp-istanbul');
 const rimraf = require('gulp-rimraf');
 const tslint = require('gulp-tslint');
-const mocha = require('gulp-mocha');
 const shell = require('gulp-shell');
 
 const outDir = 'build';
@@ -13,17 +11,8 @@ const outDir = 'build';
  * Remove build directory.
  */
 gulp.task('clean', () => {
-  return gulp.src(outDir, { read: false })
+  return gulp.src(`${outDir}`, {read: false})
     .pipe(rimraf());
-});
-
-/**
- * Create coverage report
- */
-gulp.task('coverage', () => {
-  return gulp.src([`${outDir}/src/**/*.js`, `!${outDir}/test/**/*.js`])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
 });
 
 /**
@@ -40,46 +29,10 @@ gulp.task('compile', shell.task([
 ]));
 
 /**
- * Copy config files
- */
-gulp.task('configs', (cb) => {
-  return gulp.src("src/services/config/**/*.json")
-    .pipe(gulp.dest(outDir + '/src/services/config/'));
-});
-
-/**
  * Build the project.
  */
-gulp.task('build', ['tslint', 'compile', 'configs'], () => {
-  console.log('Building the project ...');
-});
+gulp.task('build', ['tslint', 'compile']);
 
-/**
- * Run tests.
- */
-gulp.task('test', ['build', 'coverage'], () => {
-
-  gulp.src([outDir + '/test/**/*.js'])
-    .pipe(mocha())
-    .pipe(istanbul.writeReports({reportOpts: {dir: outDir + '/coverage'}}))
-    .pipe(istanbul.enforceThresholds({
-      thresholds: {
-        global: 90
-      }
-    }))
-    .once('error', (error) => {
-      console.log(error);
-      process.exit(1);
-    })
-    .once('end', () => {
-      process.exit();
-    })
-
-});
-
-/**
- * Default task for compiling project
- */
 gulp.task('default', ['clean'], () => {
   gulp.start('build');
 });
